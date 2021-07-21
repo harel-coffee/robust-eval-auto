3a3bd1bb94123824819970f3acdabf9040b8136d
JudithBernett <bernett@in.tum.de>
2021-07-21 10:25:40 +0200

    changed parameters for CPU

--- robustness_all.sh
+++ robustness_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=25
 #SBATCH --job-name=robustness1
 #SBATCH --output=%x.%j.txt
 #SBATCH --error=%x.%j.err
 #SBATCH --mem=100G
 
-python robustness_comparison/robustness_mean_jaccard.py --algorithm ROBUST --threshold 0.5 --init 0.25 --red 0.9 --threads 10 --trees 20
-python robustness_comparison/robustness_mean_jaccard.py --algorithm DIAMOND --threads 10
-python robustness_comparison/robustness_mean_jaccard.py --algorithm DOMINO --threads 10
+python robustness_comparison/robustness_mean_jaccard.py --algorithm ROBUST --threshold 0.5 --init 0.25 --red 0.9 --threads 25 --trees 20
+python robustness_comparison/robustness_mean_jaccard.py --algorithm DIAMOND --threads 25
+python robustness_comparison/robustness_mean_jaccard.py --algorithm DOMINO --threads 25
 
