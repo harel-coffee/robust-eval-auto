f0e5b7bf72b4b9ff58419941c908699bd76effc7
JudithBernett <bernett@in.tum.de>
2021-07-23 16:16:21 +0200

    test

--- robustness_comparison/utils.py
+++ robustness_comparison/utils.py
@@ -42,6 +42,7 @@ def prep_parameters(algorithm=AlgorithmSelector.ROBUST, threshold=0.5, init=0.25
     dirpath = os.path.abspath(path)
     all_files = (os.path.join(basedir, filename) for basedir, dirs, files in os.walk(dirpath) for filename in files)
     all_seeds = sorted(all_files, key = os.path.getsize)
+    #tmp
     all_seeds = [os.path.basename(file) for file in all_seeds]
     if type(threshold) == list and len(threshold) == 1:
         threshold = threshold[0]
