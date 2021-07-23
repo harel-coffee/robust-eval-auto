8364eebdb813f0ad018f0fd30a16627d7c93c3d1
JudithBernett <bernett@in.tum.de>
2021-07-23 15:49:47 +0200

    modified server files

--- robustness_all.sh
+++ robustness_all.sh
@@ -2,12 +2,10 @@
 #
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=25
-#SBATCH --job-name=robustness1
+#SBATCH --job-name=diamondrobustness
 #SBATCH --output=%x.%j.txt
 #SBATCH --error=%x.%j.err
-#SBATCH --mem=100G
+#SBATCH --mem=200G
 
-python robustness_comparison/robustness_mean_jaccard.py --algorithm ROBUST --threshold 0.5 --init 0.25 --red 0.9 --threads 25 --trees 20
 python robustness_comparison/robustness_mean_jaccard.py --algorithm DIAMOND --threads 25
-python robustness_comparison/robustness_mean_jaccard.py --algorithm DOMINO --threads 25
 
--- robustness_all_2.sh
+++ robustness_all_2.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=robustness2
 #SBATCH --output=%x.%j.txt
 #SBATCH --error=%x.%j.err
-#SBATCH --mem=100G
+#SBATCH --mem=250G
 
 python robustness_comparison/robustness_mean_jaccard.py --algorithm MUST --threads 32
 python robustness_comparison/robustness_mean_jaccard.py --algorithm RMUST --threshold 0.5 --trees 20 --threads 32
