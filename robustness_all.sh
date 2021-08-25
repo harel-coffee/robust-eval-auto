4bb8c23f75c32114f26c790a10df4832a87e8077
JudithBernett <bernett@in.tum.de>
2021-08-25 12:32:07 +0200

    bash script for sanity check

--- robustness_all.sh
+++ robustness_all.sh
@@ -2,13 +2,13 @@
 #
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=25
-#SBATCH --job-name=rob_rmust_robustness
+#SBATCH --job-name=rob_sanity
 #SBATCH --output=%x.%j.txt
 #SBATCH --error=%x.%j.err
 #SBATCH --mem=250G
 
 python robustness_comparison/robustness_mean_jaccard.py --algorithm ROBUST --threshold 0.1 --init 0.25 --red 0.9 --threads 32 --trees 30
-python robustness_comparison/robustness_mean_jaccard.py --algorithm RMUST --threshold 0.1 --trees 30 --threads 32
+#python robustness_comparison/robustness_mean_jaccard.py --algorithm RMUST --threshold 0.1 --trees 30 --threads 32
 #python robustness_comparison/robustness_mean_jaccard.py --algorithm MUST --threads 32
 #python robustness_comparison/robustness_mean_jaccard.py --algorithm DIAMOND --threads 25
 #python robustness_comparison/robustness_mean_jaccard.py --algorithm DOMINO --threads 25
